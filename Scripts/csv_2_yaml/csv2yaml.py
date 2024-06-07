bc017afdc49cc81c4efbf2ddc8b5732bf12e7dc5
benito2268 <benstaehle@gmail.com>
2024-06-07 12:14:23 -0500

    inital commit - beginnings of a CSV to YAML script and an ER diagram

---
+++ Scripts/csv_2_yaml/csv2yaml.py
@@ -0,0 +1,80 @@
+import sys
+import csv
+
+class Asset:
+
+    # each asset is represented with a nested dictionary
+    asset = {
+        'aquisition' : {
+            'po'     : '',
+            'date'   : '',
+            'reason' : '',
+        },
+
+        'purpose' : '',
+
+        'hardware' : {
+            'model'         : '',
+            'serial_number' : '',
+            'service_tag'   : '',
+        },
+
+        # TODO revisit this probably
+        'condo_chassis' : {
+            'serial_number' : '',
+            'service_tag'   : '',
+            'model'         : '',
+        },
+
+        'location' : {
+            'rack'      : '',
+            'elevation' : '',
+            'room'      : '',
+            'building'  : '',
+        }, 
+
+        'tags' : {
+            'csl'       : '',
+            'uw'        : '',
+            'morgridge' : '',
+        },
+    }
+
+    # converts an array of strings (row from the csv file) to a dictionary
+    # TODO need a way to detect condos
+    def __init__(self, csv_row):
+        print('init Asset ' + csv_row[5])
+
+# This function is meant to convert the CHTC inventory spreadsheet
+# to a yaml file - See INF-1138 in Jira
+# 
+# params: csv_name - name of the input csv file
+# returns: a list of Asset objects as read from the file
+def csv2yaml(csv_name):
+    with open(csv_name, newline='') as csvfile:
+
+        # I think the csv module considers this the "excel dialect"
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        assets = []
+
+        for row in reader:
+            print(', '.join(row))
+            # assets.append(Asset(row))
+
+    return assets
+            
+
+# having a main function might be a good idea?
+# if this module is ever imported somewhere for use of csv2yaml()
+# but this script is also kind of a one off...
+def main():
+    # take csv filename as a command line arg
+    if len(sys.argv) < 2:
+        print("usage: csv2yaml.py <csv_file>")
+        exit(1)
+
+    csv_name = sys.argv[1]
+    csv2yaml(csv_name)
+
+if __name__ == "__main__":
+    main()
