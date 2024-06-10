c3f6abc615d427620171a35c4c93340650597440
benito2268 <benstaehle@gmail.com>
2024-06-10 16:08:48 -0500

    values are now double quoted strings - via a custom YAML 'representer'

--- Scripts/csv_2_yaml/csv2yaml.py
+++ Scripts/csv_2_yaml/csv2yaml.py
@@ -2,6 +2,11 @@ import sys
 import csv
 import yaml
 
+# a wrapper class for quoted yaml string values
+# used instead of str so keys are not also quoted
+class quoted(str):
+    pass
+
 class Asset:
 
     # a shared (among all Assets) dictionary that maps YAML/dict key names
@@ -19,7 +24,7 @@ class Asset:
         'uw'            : 9,
         'csl'           : 10,
         'morgridge'     : 11,
-        'notes'         : 12, # map 'purpose' to 'notes' for now
+        'notes'         : 12,
     }
 
     # converts an array of strings (row from the csv file) to a dictionary
@@ -28,38 +33,38 @@ class Asset:
         # each asset is represented with a nested dictionary
         self.asset = {
             'aquisition' : {
-                'po'            : '',
-                'date'          : '',
-                'reason'        : '',
-                'owner'         : '',
-                'fabrication'   : '',
+                'po'            : "",
+                'date'          : "",
+                'reason'        : "",
+                'owner'         : "",
+                'fabrication'   : "",
             },
 
             'hardware' : {
-                'model'         : '',
-                'serial_number' : '',
-                'service_tag'   : '',
-                'purpose'       : '',
-                'notes'         : '',
+                'model'         : "",
+                'serial_number' : "",
+                'service_tag'   : "",
+                'purpose'       : "",
+                'notes'         : "",
             },
 
             # TODO revisit this probably
             'condo_chassis' : {
-                'identifier'    : '',
-                'model'         : '',
+                'identifier'    : "",
+                'model'         : "",
             },
 
             'location' : {
-                'rack'      : '',
-                'elevation' : '',
-                'room'      : '',
-                'building'  : '',
+                'rack'      : "",
+                'elevation' : "",
+                'room'      : "",
+                'building'  : "",
             }, 
 
             'tags' : {
-                'csl'       : '',
-                'uw'        : '',
-                'morgridge' : '',
+                'csl'       : "",
+                'uw'        : "",
+                'morgridge' : "",
             },
         }
 
@@ -68,7 +73,8 @@ class Asset:
 
         # iterate through each inner and outer key and grab
         # its corresponding value (as determined by key_map) from the spreadsheet
-        # TODO derive 'building' from 'room'
+        # TODO detect condos and fabrications (possibly via some sort of heuristic)
+        # TODO find location info in puppet repo
         # TODO this bit is in desperate need of a cleanup and fixup - but it's a start
         for outer_key, outer_value in self.asset.items():
 
@@ -76,25 +82,19 @@ class Asset:
 
                 #if outer_value is a dictionary we need to iterate one level deeper
                 for inner_key, inner_value in outer_value.items():
-                    cell = self.key_map.get(inner_key, '')
-                    if cell == '':
-                        self.asset[outer_key][inner_key] = 'MISSING'
+                    cell = self.key_map.get(inner_key, "")
+                    if cell == "":
+                        value = quoted('MISSING')
                     else:
-                        self.asset[outer_key][inner_key] = csv_row[cell]
+                        value = quoted(csv_row[cell])
 
-            else:
-                # otherwise don't iterate any more!
-                csv_val = self.key_map.get(outer_key, '')
-                if cell == '':
-                    self.asset[outer_key] = 'MISSING'
-                else:
-                    self.asset[outer_key] = csv_row[cell]
+                    self.asset[outer_key][inner_key] = value
 
 # for debugging
 def print_dict(d):
     print('\n')
     for x in d:
-        print(x, end='')
+        print(x, end="")
 
         if isinstance(x, dict):
             for y in d[x]:
@@ -107,8 +107,8 @@ def print_dict(d):
 # 
 # params: csv_name - name of the input csv file
 # returns: a list of Asset objects as read from the file
-def csv2yaml(csv_name): 
-    with open(csv_name, newline='') as csvfile:
+def csv_read(csv_name): 
+    with open(csv_name, newline="") as csvfile:
 
         # I think the csv module considers this the "excel dialect"
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -124,6 +124,12 @@ def csv2yaml(csv_name):
 
         return assets
     
+# the default Python yaml module doesn't preserve double quotes :(
+# can change that behavior with a representer
+# see "Constructors, representers, resolvers" in https://pyyaml.org/wiki/PyYAMLDocumentation
+def quote_representer(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+    
 # having a main function might be a good idea?
 # if this module is ever imported somewhere for use of csv2yaml()
 # but this script is also kind of a one off...
@@ -134,10 +140,13 @@ def main():
         exit(1)
 
     csv_name = sys.argv[1]
-    assets = csv2yaml(csv_name)
+    assets = csv_read(csv_name)
+
+    # register the yaml representer for double quoted strings
+    yaml.add_representer(quoted, quote_representer)
 
     with open(assets[0].hostname + '.' + assets[0].domain + ".yaml", 'w') as testfile:
-        yaml.dump(assets[0].asset, testfile, default_flow_style=False)
+        yaml.dump(assets[0].asset, testfile)
 
     for a in assets:
         print_dict(a.asset)
