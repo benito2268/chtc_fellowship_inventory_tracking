f931fc846bb5e1aad67259e3364e6ea44801832f
benito2268 <benstaehle@gmail.com>
2024-06-10 09:33:56 -0500

    CSV iterator now correctly skips the first row (column labels)

--- Scripts/csv_2_yaml/csv2yaml.py
+++ Scripts/csv_2_yaml/csv2yaml.py
@@ -3,6 +3,21 @@ import csv
 
 class Asset:
 
+    # a shared (among all Assets) dictionary that maps YAML/dict key names
+    # to their corresponding column number in the spreadsheet
+    key_map = {
+        'room'          : 0,
+        'rack'          : 1,
+        'elevation'     : 2,
+        'model'         : 5,
+        'serial_number' : 6,
+        'service_tag'   : 8,
+        'uw'            : 9,
+        'csl'           : 10,
+        'morgridge'     : 11,
+        'purpose'       : 12, # map 'purpose' to 'notes' for now
+    }
+
     # converts an array of strings (row from the csv file) to a dictionary
     # TODO need a way to detect condos
     def __init__(self, csv_row):
@@ -43,28 +58,63 @@ class Asset:
             },
         }
 
-        self.asset['location']['room'] = csv_row[0]
+        # iterate through each inner and outer key and grab
+        # its corresponding value (as determined by key_map) from the spreadsheet
+        # TODO derive 'building' from 'room'
+        # TODO this bit is in desperate need of a cleanup and fixup - but it's a start
+        for outer_key, outer_value in self.asset.items():
+
+            if isinstance(outer_value, dict):
+
+                #if outer_value is a dictionary we need to iterate one level deeper
+                for inner_key, inner_value in outer_value.items():
+                    cell = self.key_map.get(inner_key, '')
+                    if cell == '':
+                        self.asset[outer_key][inner_key] = 'MISSING'
+                    else:
+                        self.asset[outer_key][inner_key] = csv_row[cell]
+
+            else:
+                # otherwise don't iterate any more!
+                csv_val = self.key_map.get(outer_key, '')
+                if cell == '':
+                    self.asset[outer_key] = 'MISSING'
+                else:
+                    self.asset[outer_key] = csv_row[cell]
+
+# for debugging
+def print_dict(d):
+    print('\n')
+    for x in d:
+        print(x, end='')
+
+        if isinstance(x, dict):
+            for y in d[x]:
+                print('\t', y, ':', d[x][y])
+        else:
+            print(':', d[x])
 
 # This function is meant to convert the CHTC inventory spreadsheet
 # to a yaml file - See INF-1138 in Jira
 # 
 # params: csv_name - name of the input csv file
 # returns: a list of Asset objects as read from the file
-def csv2yaml(csv_name):
+def csv2yaml(csv_name): 
     with open(csv_name, newline='') as csvfile:
 
         # I think the csv module considers this the "excel dialect"
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         assets = []
 
+        # skip labels in the first CSV row
+        next(reader)
+
         for row in reader:
             # print(', '.join(row))
             a = Asset(row)
             assets.append(a)
 
-        for a in assets:
-            print(a.asset['location']['room'])
-
+        return assets
     
 # having a main function might be a good idea?
 # if this module is ever imported somewhere for use of csv2yaml()
@@ -76,7 +126,10 @@ def main():
         exit(1)
 
     csv_name = sys.argv[1]
-    csv2yaml(csv_name)
+    assets = csv2yaml(csv_name)
+
+    for a in assets:
+        print_dict(a.asset)
 
 if __name__ == "__main__":
     main()
