be5d26372a0e176b4dc162c1ca6071c4451abdd7
Ben Staehle <bstaehle@wisc.edu>
2024-06-18 15:25:02 -0500

    framework for validation script

--- Scripts/data_validator/validate.py
+++ Scripts/data_validator/validate.py
@@ -1,5 +1,27 @@
+# TODO is there a better way to do this?
+import sys
+import os
+sys.path.append(os.path.abspath('../yaml_read/'))
+
+import yaml_read
+
+# main validation function
+def validate_asset(asset):
+    pass
+
 def main():
-    print('hello, world')
+    if len(sys.argv) != 2:
+        print('usage: validate.py <path-to-yaml-files>')
+        exit(1)
+
+    path = sys.argv[1]
+    if not path.endswith('/'):
+        path += '/'
+
+    assets = yaml_read.read_yaml(path)
+
+    for asset in assets:
+        validate_asset(asset)
 
 if __name__ == '__main__':
     main()
