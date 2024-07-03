00d86af79bdf20abc4b0cad8239fb4c570d6ff76
Ben Staehle <bstaehle@wisc.edu>
2024-07-03 10:12:19 -0500

    sheets test now authenticates through Google Service Account

--- Scripts/sheets/sheet_test.py
+++ Scripts/sheets/sheet_test.py
@@ -1,7 +1,7 @@
 import os.path
 
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -13,32 +13,23 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 SHEET_ID = "1rKCtBd7QMCOEc3gdyKp5XzHVwlVmf-EFgtt7sFdBVgA"
 
 # Range is specified in A1 notation
-# Google sheets says the format is 'sheetname!cell1:cell2'
+# the format is 'sheetname!cell1:cell2'
+# where 'sheet' refers to a tab within a larger spreasheet
 SAMPLE_RANGE = "Sheet1!A1:7"
+
+# TODO probably not the best idea to have this - what to do with it
+SERVICE_ACCOUNT_FILE = 'token-7-3-24.json'
 
 def main():
     creds = None
 
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    if not creds or not creds.valid:
-
-        # can refresh credentials if they are expired
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            # TODO what is this part doing??
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
+    if os.path.exists(SERVICE_ACCOUNT_FILE):
+        creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
         # try is used to catch HttpError during the API call
         try:
             service = build('sheets', 'v4', credentials=creds)
-            
+ 
             # call the sheets API to get some data
             sheet = service.spreadsheets()
             result = (
