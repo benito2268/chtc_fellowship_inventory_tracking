20cacaaccdb564544a27d9d29ca6fc4c9da6feb7
Ben Staehle <bstaehle@wisc.edu>
2024-07-22 16:52:58 -0500

    clean up gen_yaml()
    - allowed there to be a filename passed to the gen_yaml function
    - cleaned up some format strings
    - added file and CSV path CLAs in asset_add.py

--- scripts/asset_add.py
+++ scripts/asset_add.py
@@ -20,14 +20,15 @@ def chk_subproc(result: subprocess.CompletedProcess):
         print(f"{result.args.join(' ')} failed")
         exit(1)
 
-def ingest_file(newname: str, path: str):
+def ingest_file(yamlname: str, path: str):
     # make sure that cp doesn't fail if path and ./newname are the same file
-    if os.path.abspath(path) == os.path.abspath(newname):
+    if os.path.abspath(path) == os.path.abspath(yamlname):
         return
 
     # copy the file into the YAML directory
     # from the config fill
-    result = subprocess.run(["cp", path, f"./{newname}"])
+    # TODO change the dir once the config is set up
+    result = subprocess.run(["cp", path, f"./{yamlname}"])
     chk_subproc(result)
 
 # TODO make a column map in the config
@@ -38,9 +39,8 @@ def ingest_csv(path: str):
     # generate the yaml files in the current directory
     # TODO change this dir once the config is set up
     csv2yaml.gen_yaml(assets, ".")
-    pass
 
-def ingest_interactive():
+def ingest_interactive(yamlname: str):
     print("interactive")
 
 def add(filename: str, args: list):
@@ -50,11 +50,14 @@ def add(filename: str, args: list):
 
     # add the new file(s)
     if args.file:
-        ingest_file(filename, "./test.chtc.wisc.edu.yaml")
+        ingest_file(filename, args.file)
     elif args.csv:
-        ingest_csv()
+        ingest_csv(filename, args.csv)
     elif args.interactive:
         ingest_interactive()
+
+    # TODO remove for testing we never want to push :)
+    exit(0)
 
     # git add/commit/push sequence
     result = subprocess.run(["git", "add", filename])
@@ -74,8 +77,8 @@ def main():
 
     # the ingestion method are mutually exclusive
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-f", "--file", help="ingest an asset via a YAML file", action="store_true")
-    group.add_argument("-c", "--csv", help="ingest one or many assets via a CSV file", action="store_true")
+    group.add_argument("-f", "--file", help="ingest an asset via a YAML file", action="store")
+    group.add_argument("-c", "--csv", help="ingest one or many assets via a CSV file", action="store")
     group.add_argument("-i", "--interactive", help="add an asset interactivly via CLI", action="store_true")
 
     # an optional argument if the domain is different than 'chtc.wisc.edu'
