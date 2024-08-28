9c47b7aa2d785f0ecc2d2842341ffc511922287a
Ben Staehle <bstaehle@wisc.edu>
2024-08-28 15:53:41 -0500

    fixed whitespace and quotes in validator

--- scripts/integrity_checker/check_data.py
+++ scripts/integrity_checker/check_data.py
@@ -21,7 +21,7 @@ import dict_utils
 import validate_tools
 import errortypes
 
-# regex to match possible ways of saying 'missing'
+# regex to match possible ways of saying "missing"
 missing_rxp = "(?i)none|missing|\\?+|^\\s*$"
 
 # checks a single asset for missing data fields
@@ -33,59 +33,59 @@ missing_rxp = "(?i)none|missing|\\?+|^\\s*$"
 def chk_single_missing(asset: yaml_io.Asset):
     bad_tags = []
 
-    # a list of keys that are exempt from 'missing' checks
+    # a list of keys that are exempt from "missing" checks
     exempt_keys = [
-        'acquisition.reason',
-        'tags.uw',
-        'tags.morgridge',
-        'tags.csl',
-        'hardware.notes',
-        'hardware.swap_reason',
-        'hardware.condo_chassis.identifier',
+        "acquisition.reason",
+        "tags.uw",
+        "tags.morgridge",
+        "tags.csl",
+        "hardware.notes",
+        "hardware.swap_reason",
+        "hardware.condo_chassis.identifier",
     ]
 
     # condo model is conditional - if condo id not present - ignore it
-    if re.fullmatch(missing_rxp, asset.get('hardware.condo_chassis.identifier')):
-        exempt_keys.append('hardware.condo_chassis.model')   
+    if re.fullmatch(missing_rxp, asset.get("hardware.condo_chassis.identifier")):
+        exempt_keys.append("hardware.condo_chassis.model")
 
-    # use a regex for ways 'missing' is said in the speadsheet
-    # i.e. '', 'none', '???', etc.
+    # use a regex for ways "missing" is said in the speadsheet
+    # i.e. "", "none", "???", etc.
     flat = dict_utils.flatten_dict(asset.asset)
     for key, value in flat.items():
         if key not in exempt_keys and re.fullmatch(missing_rxp, str(value)):
-            bad_tags.append(': '.join((key, str(value))))
+            bad_tags.append(": ".join((key, str(value))))
 
-            # MISSING will be the 'magic string'
-            asset.put(key, yaml_io.quoted('MISSING'))
+            # MISSING will be the "magic string"
+            asset.put(key, yaml_io.quoted("MISSING"))
 
     if bad_tags:
-        # write back changes we've made and return and error
+        # write back changes we"ve made and return and error
         yaml_io.write_yaml(asset, asset.filepath)
-        return errortypes.MissingDataError(asset.fqdn + '.yaml', bad_tags, 'tags are missing values')
+        return errortypes.MissingDataError(asset.fqdn + ".yaml", bad_tags, "tags are missing values")
 
     # otherwise no error - return None
     return None
 
 def chk_all_missing(assets: list):
     errs = []
-    
+
     for asset in assets:
         err = chk_single_missing(asset)
         if err:
             errs.append(err)
 
     return errs
-        
+
 # validates assets with respect to each other
 def chk_conflicting(assets: list):
 
     # list of keys we want to grab
     # adding a key here will make the next expression groups for it
     keys = [
-        'location.rack',
-        'acquisition.po',
-        'hardware.condo_chassis.identifier',
-        'tags.uw',
+        "location.rack",
+        "acquisition.po",
+        "hardware.condo_chassis.identifier",
+        "tags.uw",
     ]
 
     groups = { key:validate_tools.group_by_attrib(assets, key) for key in keys}
@@ -99,30 +99,30 @@ def chk_conflicting(assets: list):
     errs = []
 
     # check rack against condo_chassis.identifier
-    # TODO account for elevation 'ranges' instead of checking pure equality
-    location_conflicts = validate_tools.get_conflicts('location.rack', groups, 
-                                'hardware.condo_chassis.identifier', 
-                                'assets share rack-elevation without common hardware.condo_chassis.identifier')
+    # TODO account for elevation "ranges" instead of checking pure equality
+    location_conflicts = validate_tools.get_conflicts("location.rack", groups,
+                                "hardware.condo_chassis.identifier",
+                                "assets share rack-elevation without common hardware.condo_chassis.identifier")
 
     if location_conflicts != None:
         errs.extend(location_conflicts)
 
     # check condo_id against rack
-    condo_id_confls = validate_tools.get_conflicts('hardware.condo_chassis.identifier', groups, 
-                                           'location.rack', 
-                                           'assets share hardware.condo_chassis.id but show different rack-elevation') 
+    condo_id_confls = validate_tools.get_conflicts("hardware.condo_chassis.identifier", groups,
+                                           "location.rack",
+                                           "assets share hardware.condo_chassis.id but show different rack-elevation")
     if condo_id_confls != None:
         errs.extend(condo_id_confls)
 
-    
+
     # check tags.uw against hardware.condo_chassis.identifier OR acquisition.fabrication    
-    condo_tag_confls = validate_tools.get_conflicts('tags.uw', groups,
-                                    'hardware.condo_chassis.identifier',
-                                    'assets share UW tags, but do not belong to a common condo or fabrication')
+    condo_tag_confls = validate_tools.get_conflicts("tags.uw", groups,
+                                    "hardware.condo_chassis.identifier",
+                                    "assets share UW tags, but do not belong to a common condo or fabrication")
     if condo_tag_confls != None:
-        for group in groups['tags.uw']:
+        for group in groups["tags.uw"]:
             for a in group:
-                if a.get('acquisition.fabrication') != True:
+                if a.get("acquisition.fabrication") != True:
                     errs.extend(condo_tag_confls)
                     return errs
 
@@ -132,27 +132,27 @@ def chk_uw_tag(assets: list):
     errs = []
 
     for asset in assets:
-        if re.fullmatch(missing_rxp, asset.get('tags.uw')):
-            asset_date = asset.get('acquisition.date')
+        if re.fullmatch(missing_rxp, asset.get("tags.uw")):
+            asset_date = asset.get("acquisition.date")
             if re.fullmatch(missing_rxp, asset_date):
-                errs.append(errortypes.MissingDataError(asset.fqdn + '.yaml', ['tags.uw'], 'asset with no purchase date lacks UW tag'))
+                errs.append(errortypes.MissingDataError(asset.fqdn + ".yaml", ["tags.uw"], "asset with no purchase date lacks UW tag"))
                 continue
 
             # uses ISO 8601 date format (aka. yyyy-mm-dd)
-            fmt = '%Y-%m-%d'
+            fmt = "%Y-%m-%d"
             currdate = datetime.datetime.today()
             olddate = datetime.datetime.strptime(asset_date, fmt)
 
             diff = currdate - olddate
 
-            # warn about a missing UW tag if it's been more than 180 days
+            # warn about a missing UW tag if it"s been more than 180 days
             if diff.days >= 180:
-                errs.append(errortypes.MissingDataError(asset.fqdn + '.yaml', ['tags.uw'], f'asset with PO: "{asset.get("acquisition.po")}" purchased on {asset_date} lacks UW tag'))
+                errs.append(errortypes.MissingDataError(asset.fqdn + ".yaml", ["tags.uw"], f"asset with PO: '{asset.get('acquisition.po')}' purchased on {asset_date} lacks UW tag"))
 
     return errs
 
 # performs the validation and outputs the results
-# if email_addr is '' - will output to stdout otherwise
+# if email_addr is "" - will output to stdout otherwise
 # will send and email to the specified address
 #
 # params:
@@ -160,16 +160,14 @@ def chk_uw_tag(assets: list):
 #   chk_fun: the checking function - returns a list of DataError
 #   email_addr: email address to send to - if any
 #
-def output_chks(errs: list[errortypes.DataError], email_addr: str='', yaml_path: str=''):
+def output_chks(errs: list[errortypes.DataError], email_addr: str="", yaml_path: str=""):
     output = io.StringIO()
     for err in errs:
         print(err, file=output)
-        
+
     if email_addr:
-        # TODO if this becomes some sort of ci/cd or cron job - should figure
-        # out a way to avoid spamming people's inboxes
-        header = '[this message was auto-generated by validate.py]\n\n'
-        header += f'The following asset data errors were found in {yaml_path}:\n\n'
+        header = "[this message was auto-generated by validate.py]\n\n"
+        header += f"The following asset data errors were found in {yaml_path}:\n\n"
 
         # append to the err messages
         email_out = io.StringIO()
@@ -179,12 +177,12 @@ def output_chks(errs: list[errortypes.DataError], email_addr: str='', yaml_path:
         msg = email.message.EmailMessage()
         msg.set_content(email_out.getvalue())
 
-        msg['Subject'] = 'Asset Data Validation Errors'
-        msg['From'] = 'no_reply@example.com'
-        msg['To']  = email_addr
+        msg["Subject"] = "Asset Data Validation Errors"
+        msg["From"] = "no_reply@example.com"
+        msg["To"]  = email_addr
 
         # send the message via a (very briefly alive) local SMTP server
-        s = smtplib.SMTP('localhost')
+        s = smtplib.SMTP("localhost")
         s.send_message(msg)
         s.quit()
 
@@ -201,10 +199,10 @@ def main():
     parser = argparse.ArgumentParser()
 
     # add new options here
-    parser.add_argument('-m', '--missing', help='only check for asset tags that are missing values', action='store_true')
-    parser.add_argument('-c', '--conflicting', help='only check for conflicting asset data', action='store_true')
-    parser.add_argument('-u', '--uwtag', help='check for missing UW tags on assets older than 180 days', action='store_true')
-    parser.add_argument('-p', '--path', help='the path to a directory containing YAML asset files to validate', type=str)
+    parser.add_argument("-m", "--missing", help="only check for asset tags that are missing values", action="store_true")
+    parser.add_argument("-c", "--conflicting", help="only check for conflicting asset data", action="store_true")
+    parser.add_argument("-u", "--uwtag", help="check for missing UW tags on assets older than 180 days", action="store_true")
+    parser.add_argument("-p", "--path", help="the path to a directory containing YAML asset files to validate", type=str)
 
     args = parser.parse_args()
 
@@ -212,12 +210,12 @@ def main():
     # keys match the long command-line options
     # it should invoke
     validate_funcs = {
-        'missing'     : chk_all_missing,
-        'conflicting' : chk_conflicting,
-        'uwtag'       : chk_uw_tag,
+        "missing"     : chk_all_missing,
+        "conflicting" : chk_conflicting,
+        "uwtag"       : chk_uw_tag,
     }
 
-    # scripts can be run 'manually' too
+    # scripts can be run "manually" too
     yaml_path = ""
     if args.path:
         yaml_path = args.path
@@ -244,5 +242,5 @@ def main():
 
     output = output_chks(errs, args.email, yaml_path)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
